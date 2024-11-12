3b5b11b25ac9d8599d169a070b18fc7ec7727f50
JFFA25 <230417@utxicotepec.edu.mx>
2024-11-11 19:13:09 -0600

    Practica12
    Mostrando los Errores en la Pantalla

--- Controllers/usuarioController.js
+++ Controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import { check ,validationResult } from 'express-validator'
 import Usuario from '../Models/Usuario.js'
 const formularioLogin = (req,res) => {
     res.render('auth/login',{
@@ -6,13 +7,31 @@ const formularioLogin = (req,res) => {
 }
 const formularioRegistro = (req,res) => {
     res.render('auth/registro',{
-        pagina : 'Crear cuenta'
+        pagina : 'Crear Cuenta'
     })
 }
 const registrar = async (req,res) => {
-   const usuario = await Usuario.create(req.body)
+        //Validacion 
+        await check('nombre').notEmpty().withMessage('El Nombre no puede ir vacio').run(req)
+        await check('email').isEmail().withMessage('Eso no parece un email').run(req)
+        await check('password').isLength({min:8}).withMessage('La contraseña debe de ser minimo 8 caracteres').run(req)
+        await check('repeat_password').equals('password').withMessage('Las contraseñas no coinciden').run(req)
+        
+        let resultado = validationResult(req)
+    
+        //Verificar que el resulatado este vacio
+        if(!resultado.isEmpty()){
+            //Errores
+            return res.render('auth/registro',{
+                pagina : 'Crear Cuenta',
+                errores: resultado.array()
+            })
 
-   res.json(usuario)
+        }
+        res.json()
+
+        const usuario = await Usuario.create(req.body);
+        res.json(usuario)
 }
 const formularioPassword = (req,res) => {
     res.render('auth/password',{
