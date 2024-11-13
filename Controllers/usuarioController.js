5b12e3daa6fbdf24ca8922954ab722f58277311a
JFFA25 <230417@utxicotepec.edu.mx>
2024-11-12 19:23:21 -0600

    Practica12
    Auto llenando los campos que pasaron la validación

--- Controllers/usuarioController.js
+++ Controllers/usuarioController.js
@@ -11,10 +11,11 @@ const formularioRegistro = (req,res) => {
     })
 }
 const registrar = async (req,res) => {
+    
         //Validacion 
         await check('nombre').notEmpty().withMessage('El Nombre no puede ir vacio').run(req)
         await check('email').isEmail().withMessage('Eso no parece un email').run(req)
-        await check('password').isLength({min:8}).withMessage('La contraseña debe de ser minimo 8 caracteres').run(req)
+        await check('password').isLength({min:4}).withMessage('La contraseña debe de ser minimo 8 caracteres').run(req)
         await check('repeat_password').equals('password').withMessage('Las contraseñas no coinciden').run(req)
         
         let resultado = validationResult(req)
@@ -24,7 +25,11 @@ const registrar = async (req,res) => {
             //Errores
             return res.render('auth/registro',{
                 pagina : 'Crear Cuenta',
-                errores: resultado.array()
+                errores: resultado.array(),
+                usuario: {
+                    nombre : req.body.nombre,
+                    email : req.body.email
+                }               
             })
 
         }
